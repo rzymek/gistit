2c1a0a8c29fcf577f279587c817cad428e3c2aa7
rzymek <rzymek@gmail.com>
2014-05-07 23:20:46 +0200

    App

---
+++ src/pl/rzymek/gistit/App.java
@@ -0,0 +1,82 @@
+package pl.rzymek.gistit;
+
+import java.io.IOException;
+
+import retrofit.ErrorHandler;
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.android.AndroidApacheClient;
+import retrofit.client.Client;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.app.Application;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+public class App extends Application {
+	private static final String ACCOUNT_TYPE = "com.github";
+	public GitHubService github;
+	protected String token;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Client client = new AndroidApacheClient();
+		RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.github.com").setRequestInterceptor(new RequestInterceptor() {
+			@Override
+			public void intercept(RequestFacade request) {
+				if (token != null)
+					request.addHeader("Authorization", "token " + token);
+			}
+		}).setErrorHandler(new ErrorHandler() {
+			@Override
+			public Throwable handleError(RetrofitError error) {
+				Log.e("RETROFIT", error + "\n" + error.getCause());
+				return error.getCause();
+			}
+		}).setClient(client).build();
+
+		github = restAdapter.create(GitHubService.class);
+		fetchGithubAuthToken();
+	}
+
+	private void fetchGithubAuthToken() {
+		AccountManager service = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+		Account[] accounts = service.getAccountsByType(ACCOUNT_TYPE);
+		if (accounts.length == 0) {
+			Log.e("GET_TOKEN", "no github account");
+			return;
+		}
+//		if(accounts.length > 1)
+//			showAccountSelector
+		Account account = accounts[0];
+
+		@SuppressWarnings("deprecation")
+		final AccountManagerFuture<Bundle> future = service.getAuthToken(account, ACCOUNT_TYPE, true, null, null);
+
+		new AsyncTask<Void, Void, String>() {
+
+			@Override
+			protected String doInBackground(Void... params) {
+				try {
+					Bundle result = future.getResult();
+					return result != null ? result.getString(android.accounts.AccountManager.KEY_AUTHTOKEN) : null;
+				} catch (OperationCanceledException | AuthenticatorException | IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+
+			protected void onPostExecute(String result) {
+				token = result;
+			};
+		}.execute();
+
+	}
+}
--- src/pl/rzymek/gistit/GistIt.java
+++ src/pl/rzymek/gistit/GistIt.java
@@ -1,101 +1,41 @@
 package pl.rzymek.gistit;
 
-import java.io.IOException;
-
-import retrofit.ErrorHandler;
-import retrofit.RequestInterceptor;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class GistIt extends ActionBarActivity {
 
-	private GitHubService github;
 	private TextView newText;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gist_it);
 		newText = (TextView) findViewById(R.id.newText);
-
-		RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.github.com").setRequestInterceptor(new RequestInterceptor() {
-			@Override
-			public void intercept(RequestFacade request) {
-				Log.w("header", "" + token);
-				request.addHeader("Authorization", "token " + token);
-			}
-		}).setErrorHandler(new ErrorHandler() {
-			@Override
-			public Throwable handleError(RetrofitError error) {
-				Log.e("RETROFIT", error+"\n"+error.getCause());
-				return error.getCause();
-			}
-		})
-
-		.build();
-		github = restAdapter.create(GitHubService.class);
-		getGithubAuthToken();
+		progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
 		if (savedInstanceState == null) {
 			Intent intent = getIntent();
 			if (intent != null) {
 				if (Intent.ACTION_SEND.equals(intent.getAction())) {
 					String message = defaultString(intent.getStringExtra(Intent.EXTRA_TEXT), "");
-					String subject = defaultString(intent.getStringExtra(Intent.EXTRA_SUBJECT), "");
+					String subject = intent.getStringExtra(Intent.EXTRA_SUBJECT);
 					String msg = TextUtils.isEmpty(subject) ? message : "[" + subject + "](" + message + ")";
-					updateGistTask.execute(msg);
 					newText.setText(msg);
+					updateGistTask.execute(msg);
 				}
 			}
 		}
-	}
-
-	private String token;
-
-	private void getGithubAuthToken() {
-		new AsyncTask<Void, Void, String>() {
-			@Override
-			protected String doInBackground(Void... params) {
-				Log.w("TOKEN", "github login");
-				AccountManager service = (AccountManager) getSystemService(ACCOUNT_SERVICE);
-				Account[] accounts = service.getAccountsByType("com.github");
-				if (accounts.length == 0) {
-					return "no github account";
-				}
-				Account account = accounts[0];
-				AccountManagerFuture<Bundle> future = service.getAuthToken(account, "com.github", true, null, null);
-
-				try {
-					Bundle result = future.getResult();
-					return result != null ? result.getString(android.accounts.AccountManager.KEY_AUTHTOKEN) : null;
-				} catch (OperationCanceledException | AuthenticatorException | IOException e) {
-					e.printStackTrace();
-					return e.toString();
-				}
-			}
-
-			protected void onPostExecute(String result) {
-				token = result;
-				Log.w("TOKEN", "" + result);
-			};
-
-		}.execute();
-
 	}
 
 	private String defaultString(String value, String defaultValue) {
@@ -104,7 +44,6 @@ public class GistIt extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.gist_it, menu);
 		return true;
@@ -117,30 +56,35 @@ public class GistIt extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_send) {
-			sendGist();
+			updateGistTask.execute(newText.getText().toString());
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void sendGist() {
-		updateGistTask.execute(newText.getText().toString());
-	}
-
-	private AsyncTask<String, Void, String> updateGistTask = new AsyncTask<String, Void, String>() {
+	private AsyncTask<String, Void, Void> updateGistTask = new AsyncTask<String, Void, Void>() {
 		@Override
-		protected String doInBackground(String... params) {
-			// String newText = params[0];
+		protected void onPreExecute() {
+			progressBar.setVisibility(View.VISIBLE);
+			newText.setEnabled(false);
+		};
+
+		@Override
+		protected Void doInBackground(String... params) {
+			App app = (App) getApplication();
 			String id = "4299973c43fa6964bce1";
-			Gist gist = github.getGist(id);
+			Gist gist = app.github.getGist(id);
 			gist.getDefaultFile().content += "  \n" + params[0];
-			github.update(id, gist);
-			return "";
+			app.github.update(id, gist);
+			return null;
 		}
 
-		protected void onPostExecute(String result) {
-			finish();
+		@Override
+		protected void onPostExecute(Void result) {
 			Toast.makeText(GistIt.this, "Gist updated", Toast.LENGTH_SHORT).show();
+			finish();
+			progressBar.setVisibility(View.INVISIBLE);
+			newText.setEnabled(true);
 		};
 	};
 
