3622fd484873285b90a9afd5beca61a2bf872a1b
rzymek <rzymek@gmail.com>
2014-05-08 16:48:22 +0200

    account selector

--- src/org/gistit/AuthRequestResult.java
+++ src/org/gistit/AuthRequestResult.java
@@ -3,6 +3,6 @@ package org.gistit;
 import android.content.Intent;
 
 public interface AuthRequestResult {
-	public void allowed(String token);
+	public void allowed(String token); 
 	public void denied(Intent intent);
 }
