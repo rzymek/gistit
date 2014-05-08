07206663ff40300442e219450d4ab4310f566de0
rzymek <rzymek@gmail.com>
2014-05-08 16:32:20 +0200

    lunch google play if no github accounts

--- src/org/gistit/Authenticator.java
+++ src/org/gistit/Authenticator.java
@@ -12,21 +12,23 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
 public class Authenticator {
-	private Activity activity;
+	private Activity parent;
 
 	public Authenticator(Activity activity) {
-		this.activity = activity;
+		this.parent = activity;
 	}
+
 	@SuppressWarnings("deprecation")
 	public void fetchGithubAuthTokenUI(final AuthRequestResult callback) {
-		final App app = (App) activity.getApplication();
+		final App app = (App) parent.getApplication();
 		if (app.token != null)
 			callback.allowed(app.token);
-		AccountManager service = (AccountManager) activity.getSystemService(Activity.ACCOUNT_SERVICE);
+		AccountManager service = (AccountManager) parent.getSystemService(Activity.ACCOUNT_SERVICE);
 		final String ACCOUNT_TYPE = "com.github";
 		Account[] accounts = service.getAccountsByType(ACCOUNT_TYPE);
 		if (accounts.length == 0) {
@@ -53,54 +55,29 @@ public class Authenticator {
 						}
 					}
 				} catch (OperationCanceledException | AuthenticatorException | IOException e) {
-					Log.wtf("AUTH",""+e);
-				}
-			}
-		}, null);
-	}
-
-	@SuppressWarnings("deprecation")
-	public void fetchGithubAuthToken(final Runnable onTokenReady) {
-		final App app = (App) activity.getApplication();
-		if (app.token != null)
-			onTokenReady.run();
-		AccountManager service = (AccountManager) activity.getSystemService(Activity.ACCOUNT_SERVICE);
-		final String ACCOUNT_TYPE = "com.github";
-		Account[] accounts = service.getAccountsByType(ACCOUNT_TYPE);
-		if (accounts.length == 0) {
-			showInstallGithubDialog();
-			return;
-		}
-		// if(accounts.length > 1)
-		// showAccountSelector
-		Account account = accounts[0];
-
-		service.getAuthToken(account, ACCOUNT_TYPE, true, new AccountManagerCallback<Bundle>() {
-
-			@Override
-			public void run(AccountManagerFuture<Bundle> future) {
-				try {
-					Bundle result = future.getResult();
-					if (result != null) {
-						app.token = result.getString(android.accounts.AccountManager.KEY_AUTHTOKEN);
-						onTokenReady.run();
-					}
-				} catch (OperationCanceledException | AuthenticatorException | IOException e) {
-					e.printStackTrace();
+					Log.wtf("AUTH", "" + e);
 				}
 			}
 		}, null);
 	}
 
 	private void showInstallGithubDialog() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		AlertDialog.Builder builder = new AlertDialog.Builder(parent);
 		builder.setTitle("No GitHub account configured");
 		builder.setMessage("Install GitHub Mobile application");
 		builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				// go to play store
+				String appPackageName = "com.github.mobile";
+				try {
+					Uri uri = Uri.parse("market://details?id=" + appPackageName);
+					parent.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+				} catch (android.content.ActivityNotFoundException anfe) {
+					Uri uri = Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName);
+					parent.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+				}
+
 			}
 
 		});
@@ -108,10 +85,9 @@ public class Authenticator {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				activity.finish();
+				parent.finish();
 			}
 		});
 		builder.create().show();
 	}
-
 }
